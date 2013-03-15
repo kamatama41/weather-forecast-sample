fc8faef769292e86b801942318e2df3f369a4519
kamatama41 <shiketaudonko41@yahoo.co.jp>
2013-03-15 18:48:21 +0900

    first commit

---
+++ test/unit/com/kamatama41/sample/WeatherServiceTests.groovy
@@ -0,0 +1,15 @@
+package com.kamatama41.sample
+
+import com.kamatama41.sample.weather.WeatherService
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(WeatherService)
+class WeatherServiceTests {
+
+	void testSomething() {
+		fail "Implement me"
+	}
+}
